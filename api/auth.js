430feb5a21af770efd68c4629f54fff9f9cd5c98
ducviet-mis <11258910@st.neu.edu.vn>
2026-07-15 18:13:32 +0700

    Delete api directory

--- api/auth.js
+++
@@ -1,9 +0,0 @@
-// api/auth.js
-export default function handler(req, res) {
-  const client_id = process.env.OAUTH_CLIENT_ID;
-  const redirect_uri = `https://${req.headers.host}/api/callback`;
-  
-  res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user&redirect_uri=${redirect_uri}`
-  );
-}
